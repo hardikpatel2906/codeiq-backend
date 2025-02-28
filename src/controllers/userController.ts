57e231ec33c508bce2d06c827529d0d1995f4cee
Hardik Patel <h.patel290695@gmail.com>
2025-02-27 22:50:10 -0500

    Generate token with APIs

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import User from "../models/userModel";
 
+
+/**
+ * GENERATE JWT TOKEN  
+*/
+const generateToken = (id: string): string => {
+    return jwt.sign({ id }, process.env.JWT_SECRET as string, {
+        expiresIn: "7d",
+    });
+};
 
 /** REGISTER USER */
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
@@ -21,7 +31,7 @@ export const registerUser = async (req: Request, res: Response): Promise<void> =
                 _id: newUser.id,
                 fullName: newUser.fullName,
                 email: newUser.email,
-                // token: generateToken(newUser.id),
+                token: generateToken(newUser.id),
             });
         } else {
             res.status(400).json({ message: "Invalid user data" });
@@ -47,12 +57,13 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
                 _id: user.id,
                 fullName: user.fullName,
                 email: user.email,
-                //   token: generateToken(user.id),
+                  token: generateToken(user.id),
             });
         } else {
             res.status(401).json({ message: "Invalid email or password" });
         }
     } catch (error) {
+        // console.log(error);
         res.status(500).json({ message: "Server error", error });
     }
 };
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import route from './routes';
 dotenv.config();
 
 const app = express();
+app.use(cors());
+
 app.use(express.json())
 app.use(route);
 
